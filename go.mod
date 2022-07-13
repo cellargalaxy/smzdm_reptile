9dd32ca03b065ca59ae23904d8e45e09772769b9
lixinzhao <lixinzhao@bytedance.com>
2022-07-13 19:55:02 +0800

    备份

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
-	github.com/cellargalaxy/go_common v0.0.0-20220709131439-f2f57e1d43e1
-	github.com/cellargalaxy/msg_gateway v0.0.0-20220709154412-8f563df31864
-	github.com/cellargalaxy/server_center v0.0.0-20220709152844-594d7a61ee16
+	github.com/cellargalaxy/go_common v0.0.0-20220713114646-eeba45e55076
+	github.com/cellargalaxy/msg_gateway v0.0.0-20220713115249-5f3dad292b4e
+	github.com/cellargalaxy/server_center v0.0.0-20220713114935-7b71379126bc
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible
--- spider/spider.go
+++ spider/spider.go
@@ -2,13 +2,9 @@ package spider
 
 import (
 	"github.com/cellargalaxy/go_common/util"
-	"github.com/cellargalaxy/smzdm_reptile/config"
 	"github.com/go-resty/resty/v2"
 	"regexp"
-	"time"
 )
-
-const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
 
 var httpClient *resty.Client
 
@@ -17,7 +13,7 @@ var dateRegexp *regexp.Regexp
 
 func init() {
 	var err error
-	httpClient = util.CreateHttpClient(config.Config.Timeout, config.Config.Sleep, time.Minute*5, config.Config.Retry, map[string]string{"User-Agent": userAgent}, true)
+	httpClient = util.GetHttpClientRetry()
 
 	numRegexp, err = regexp.Compile("\\d+(\\.\\d+)*")
 	if err != nil {
